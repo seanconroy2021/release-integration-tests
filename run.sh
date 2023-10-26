d7b9fe2e0c60cac77e383c5e893d27b9e11c88ff
Leandro Mendes <lmendes@redhat.com>
2023-10-26 16:15:28 +0200

    adds local

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #
 #
-set -o pipefail
+set -e -o pipefail
 
 PID=$$
 TEMP=/tmp/$PID
 
 function print_help() {
-    echo -e "$0\t -t|--test <TEST TO RUN>"
+    echo -e "$0\t -t|--test <test name>\tTest name to run"
+    echo -e "\t -l|--local\tWeather to run test locally"
+    echo -e "\t -a|--address <cluster address>\tCluster auth address"
+    echo -e "\t -u|--username <username>\tAuth username"
 }
 
-options=$(getopt -o t:a:u: --long test:,address:,username: -- "$@")
+options=$(getopt -o lt:a:u: --long local,test:,address:,username: -- "$@")
 eval set -- "$options"
+
+
 while true; do
     case "$1" in
         -t|--test)
@@ -22,7 +27,10 @@ while true; do
             shift
             AUTH_HOST=$1
             ;;
-
+        -l|--local)
+            shift 
+            LOCAL=true
+            ;;
         -u|--username)
             shift
             USERNAME=$1
@@ -42,20 +50,26 @@ if [ ! -d "$TEST_DIR" ]; then
     exit 1
 fi
 
-# set Kubeconfig for dev and managed namespace:
+echo -e "==== Running [ `echo $TEST_DIR |tr a-z A-Z` ] test ====\n"
 
-[ -z "$USERNAME" ] && read -p "Username: " USERNAME
-read -p "Password: " -s PASSWORD
-DEV_KUBECONFIG=${TEMP}-dev-kubeconfig
-MANAGED_KUBECONFIG=${TEMP}-managed-kubeconfig
+if [ -n "$LOCAL" ]; then
 
-export DEV_KUBECONFIG MANAGED_KUBECONFIG
-touch $DEV_KUBECONFIG $MANAGED_KUBECONFIG
-oc login $AUTH_HOST -u $USERNAME -p "${PASSWORD}"  --insecure-skip-tls-verify=true --kubeconfig $DEV_KUBECONFIG
-oc project dev-release-team-tenant --kubeconfig $DEV_KUBECONFIG
+    echo -e "Running tests locally as \`$USERNAME\`"
 
-oc login $AUTH_HOST -u $USERNAME -p "${PASSWORD}"  --insecure-skip-tls-verify=true --kubeconfig $MANAGED_KUBECONFIG
-oc project managed-release-team-tenant --kubeconfig $MANAGED_KUBECONFIG
+    # set Kubeconfig for dev and managed namespace:
+    [ -z "$USERNAME" ] && read -p "Username: " USERNAME
+    read -p "Password: " -s PASSWORD
+    DEV_KUBECONFIG=${TEMP}-dev-kubeconfig
+    MANAGED_KUBECONFIG=${TEMP}-managed-kubeconfig
+    
+    export DEV_KUBECONFIG MANAGED_KUBECONFIG
+    touch $DEV_KUBECONFIG $MANAGED_KUBECONFIG
+    oc login $AUTH_HOST -u $USERNAME -p "${PASSWORD}"  --insecure-skip-tls-verify=true --kubeconfig $DEV_KUBECONFIG
+    oc project dev-release-team-tenant --kubeconfig $DEV_KUBECONFIG
+    
+    oc login $AUTH_HOST -u $USERNAME -p "${PASSWORD}"  --insecure-skip-tls-verify=true --kubeconfig $MANAGED_KUBECONFIG
+    oc project managed-release-team-tenant --kubeconfig $MANAGED_KUBECONFIG
+fi
 
 cd $TEST_DIR
 sh test.sh
